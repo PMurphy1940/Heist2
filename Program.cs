2b0f30abfb7e7f0ef0fa8338bd6d9a31c74bac30
Patrick Murphy <engine_ear@mac.com>
2020-09-04 15:21:54 -0500

    Rolodex sorts by role and displays

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Heist2 {
     partial class Program {
         static void Main (string[] args) {
-            Console.Clear();
+            Console.Clear ();
             Console.WriteLine ("Hello Bankheist!");
 
             Hacker patrick = new Hacker () {
@@ -71,17 +72,54 @@ namespace Heist2 {
                 jonas
             };
 
-            List<string> specailities = new List<string> (){
-                "Hacker (Disables alarms)", "Muscle (Disarms guards)", "Lock Specialist (Cracks vault)"
+            List<string> specailities = new List<string> () {
+                "Hacker (Disables alarms)",
+                "Muscle (Disarms guards)",
+                "Lock Specialist (Cracks vault)"
             };
-            foreach (IRobber robber in rolodex) 
-            {
+
+            printRolodex (rolodex);
+
+        }
+
+        static void printRolodex (List<IRobber> rolodex) {
+            static string job (IRobber robber) {
                 string Target = robber.ToString ();
                 string[] SplitTarget = Target.Split (".");
-                Console.WriteLine($"{robber.Name}: {SplitTarget[1]}, Skill Level {robber.SkillLevel}, Cut {robber.PercentageCut}%.");
+                return SplitTarget[1];
             }
+            List<IRobber> sortedRolodex = rolodex.OrderBy (a => job (a)).ToList ();
+            // rolodex.Sort((a, b) => string.Compare(a.))
+            Console.Clear ();
+            Console.WriteLine ("Current Rolodex");
+            Console.WriteLine ("---------------");
+            foreach (IRobber robber in rolodex) {
+                string tab = "\t";
+                string Target = robber.ToString ();
+                string[] SplitTarget = Target.Split (".");
+                int namelength = robber.Name.Length;
+                int specialitylength = SplitTarget[1].Length;
+                string col1 = tab + tab;
+                string col2 = tab + tab;
+                string col3 = tab;
 
+                if (namelength >= 7) {
+                    col1 = tab;
+                }
+                if (specialitylength >= 9) {
+                    col2 = tab;
+                }
+                if (SplitTarget[1] == "Hacker") {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                } else if (SplitTarget[1] == "Muscle") {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                } else if (SplitTarget[1] == "LockSpecialist") {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
 
+                Console.WriteLine ($"{robber.Name}:{col1}{SplitTarget[1]}, {col2}Skill Level {robber.SkillLevel}, {col3}Cut {robber.PercentageCut}%.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
         }
     }
 }
